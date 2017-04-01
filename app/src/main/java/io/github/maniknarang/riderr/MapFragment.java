647e3b89fd651816495e423069320f02432bab0c
prgzz <p1gautam@ucsd.edu>
2017-04-01 03:00:50 -0500

    Updated map

--- app/src/main/java/io/github/maniknarang/riderr/MapFragment.java
+++ app/src/main/java/io/github/maniknarang/riderr/MapFragment.java
@@ -1,9 +1,13 @@
 package io.github.maniknarang.riderr;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -17,6 +21,12 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
         GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMapLongClickListener,
         GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener
 {
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+    private final int[] MAP_TYPES = {GoogleMap.MAP_TYPE_SATELLITE, GoogleMap.MAP_TYPE_NORMAL,
+                                     GoogleMap.MAP_TYPE_HYBRID, GoogleMap.MAP_TYPE_TERRAIN,
+                                     GoogleMap.MAP_TYPE_NONE };
+    private int curMapTypeIndex = 0;
     @Override
     public void onConnected(Bundle bundle)
     {
@@ -57,5 +67,25 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
     public boolean onMarkerClick(Marker marker)
     {
         return false;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
+        mGoogleApiClient = new GoogleApiClient.Builder( getActivity() )
+                .addConnectionCallbacks( this )
+                .addOnConnectionFailedListener( this )
+                .addApi( LocationServices.API )
+                .build();
+        initListeners();
+    }
+
+    private void initListeners() {
+        getMap().setOnMarkerClickListener(this);
+        getMap().setOnMapLongClickListener(this);
+        getMap().setOnInfoWindowClickListener( this );
+        getMap().setOnMapClickListener(this);
     }
 }
