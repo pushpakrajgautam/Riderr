2165482497d0944f663293d79c216e520f83c4b9
prgzz <p1gautam@ucsd.edu>
2017-04-01 13:00:44 -0500

    Results progress

--- app/src/main/java/io/github/maniknarang/riderr/ResultActivity.java
+++ app/src/main/java/io/github/maniknarang/riderr/ResultActivity.java
@@ -1,12 +1,33 @@
 package io.github.maniknarang.riderr;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ResultActivity extends AppCompatActivity
 {
@@ -16,7 +37,8 @@ public class ResultActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String url = intent.getStringExtra("JsonUrl");
-        
+        DownloadTask downloadTask = new DownloadTask();
+        downloadTask.execute(url);
     }
 
     @Override
@@ -30,4 +52,105 @@ public class ResultActivity extends AppCompatActivity
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private class DownloadTask extends AsyncTask<String, Void, String>
+    {
+        @Override
+        protected String doInBackground(String... url)
+        {
+            // For storing data from web service
+            String data = "";
+
+            try
+            {
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+            }
+            catch(Exception e)
+            {
+                Log.d("Background Task",e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s)
+        {
+            super.onPostExecute(s);
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(s);
+        }
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, String[]>
+    {
+
+        @Override
+        protected String[] doInBackground(String... jsonData)
+        {
+            JSONObject jObject;
+            JSONArray routes=null;
+            try
+            {
+                jObject = new JSONObject(jsonData[0]);
+                routes = jObject.getJSONArray("routes");
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] lists)
+        {
+            super.onPostExecute(lists);
+        }
+
+    }
+
+    private String downloadUrl(String strUrl) throws IOException
+    {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while( ( line = br.readLine()) != null){
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        }catch(Exception e)
+        {
+        }
+        finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
 }
