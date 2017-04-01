99cf5a04f20f91822f928353cdf2a52f089681d3
prgzz <p1gautam@ucsd.edu>
2017-04-01 04:40:58 -0500

    Proper map

--- app/src/main/java/io/github/maniknarang/riderr/MapFragment.java
+++ app/src/main/java/io/github/maniknarang/riderr/MapFragment.java
@@ -1,30 +1,86 @@
 package io.github.maniknarang.riderr;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
 
 public class MapFragment extends SupportMapFragment implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMapLongClickListener,
-        GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener
+        GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener, LocationListener
 {
     private GoogleApiClient mGoogleApiClient;
     private Location mCurrentLocation;
     private final int[] MAP_TYPES = {GoogleMap.MAP_TYPE_SATELLITE, GoogleMap.MAP_TYPE_NORMAL,
                                      GoogleMap.MAP_TYPE_HYBRID, GoogleMap.MAP_TYPE_TERRAIN,
                                      GoogleMap.MAP_TYPE_NONE };
-    private int curMapTypeIndex = 0;
+    private int curMapTypeIndex = 1;
+    private LocationRequest mLocationRequest;
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        if(mGoogleApiClient != null && mGoogleApiClient.isConnected())
+            mGoogleApiClient.disconnect();
+    }
+
     @Override
     public void onConnected(Bundle bundle)
+    {
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mCurrentLocation == null)
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        else
+            handleLocation(mCurrentLocation);
+        initCamera(mCurrentLocation);
+    }
+
+    private void initCamera(Location mCurrentLocation)
+    {
+        CameraPosition cameraPosition = CameraPosition.builder().target(new LatLng(mCurrentLocation.getLatitude(),
+                                        mCurrentLocation.getLongitude())).zoom(16f).bearing(0.0f).tilt(0.0f).build();
+        getMap().animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), null);
+        getMap().setMapType( MAP_TYPES[curMapTypeIndex] );
+        getMap().setTrafficEnabled( true );
+        getMap().setMyLocationEnabled( true );
+        getMap().getUiSettings().setZoomControlsEnabled( true );
+    }
+
+    private void handleLocation(Location mCurrentLocation)
     {
 
     }
@@ -32,13 +88,13 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
     @Override
     public void onConnectionSuspended(int i)
     {
-
+        Toast.makeText(getContext(), "Suspended", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult)
     {
-
+        Toast.makeText(getContext(), "Failed", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -50,19 +106,49 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
     @Override
     public void onMapClick(LatLng latLng)
     {
+        MarkerOptions options = new MarkerOptions().position( latLng );
+        options.title( getAddressFromLatLng( latLng ) );
 
+        options.icon( BitmapDescriptorFactory.defaultMarker() );
+        getMap().addMarker( options );
+    }
+
+    private String getAddressFromLatLng( LatLng latLng )
+    {
+        Geocoder geocoder = new Geocoder( getActivity() );
+
+        String address = "";
+        try
+        {
+            address = geocoder
+                    .getFromLocation( latLng.latitude, latLng.longitude, 1 )
+                    .get( 0 ).getAddressLine( 0 );
+        }
+        catch (IOException e )
+        {
+        }
+
+        return address;
     }
 
     @Override
     public void onMapLongClick(LatLng latLng)
     {
+        MarkerOptions options = new MarkerOptions().position( latLng );
+        options.title( getAddressFromLatLng( latLng ) );
 
+        options.icon( BitmapDescriptorFactory.fromBitmap(
+                BitmapFactory.decodeResource( getResources(),
+                        R.mipmap.ic_launcher ) ) );
+
+        getMap().addMarker( options );
     }
 
     @Override
     public boolean onMarkerClick(Marker marker)
     {
-        return false;
+        marker.showInfoWindow();
+        return true;
     }
 
     @Override
@@ -78,10 +164,67 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
         initListeners();
     }
 
-    private void initListeners() {
+    private void initListeners()
+    {
         getMap().setOnMarkerClickListener(this);
         getMap().setOnMapLongClickListener(this);
         getMap().setOnInfoWindowClickListener( this );
         getMap().setOnMapClickListener(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        handleLocation(location);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED)
+        {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case 101:
+            {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    mLocationRequest = LocationRequest.create()
+                            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                            .setInterval(10 * 1000)
+                            .setFastestInterval(1 * 1000);
+
+                }
+                else
+                {
+
+                }
+                return;
+            }
+        }
     }
 }
